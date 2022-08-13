9d11c2fc014e737f40d30e1e4a8f4573fdf18b89
Mr-G-D <daredinesh00@gmail.com>
2022-08-13 11:18:23 +0530

    :boom: delete method added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,7 +4,7 @@ import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDING_ID,
